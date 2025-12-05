a7a79f4e750a12086828c4769a91a501b26f79c7
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2025-12-06 01:06:56 +0530

    added post route

--- server.js
+++ server.js
@@ -16,3 +16,20 @@ app.get("/api/sample", (req, res) => {
     { id: 2, name: "Sharma tanish" },
   ]);
 });
+
+// post route
+
+let users = [
+  { id: 1, name: "Tanish sharma" },
+  { id: 2, name: "Sharma tanish" },
+];
+
+app.post("/api/users", (req, res) => {
+  const newUser = {
+    id: users.length + 1,
+    name: req.body.name,
+  };
+
+  users.push(newUser);
+  res.send(201).send(newUser);
+});
